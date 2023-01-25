d298d8d8181c5fd0f2b228a07df536235b3092b0
amtownse <alexander.townsend@uvm.edu>
2023-01-24 19:31:47 -0500

    Finished joints

--- simulate.py
+++ simulate.py
@@ -1,10 +1,15 @@
 import pybullet as p
+import pybullet_data
 import time
 
 def main():
     physicsClient = p.connect(p.GUI)
-    p.loadSDF("box.sdf")
-    for i in range(1000):
+    p.setAdditionalSearchPath(pybullet_data.getDataPath())
+    p.setGravity(0,0,-9.8)
+    planeId = p.loadURDF("plane.urdf")
+    p.loadSDF("world.sdf")
+    robotId = p.loadURDF("body.urdf")
+    for i in range(100000):
         if i%100==0:
             print(i)
         p.stepSimulation()
