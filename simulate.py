fc741ee3368e4b19cd176484f5caa2ab408d7af8
amtownse <alexander.townsend@uvm.edu>
2023-01-31 08:20:55 -0500

    added a sinosodial function to control motors

--- simulate.py
+++ simulate.py
@@ -3,8 +3,17 @@ import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import numpy
 import time
+import random as r
 
 def main():
+    BLamp = numpy.pi/3
+    BLfeq = 1.7
+    BLoff = 0.01
+    FLamp = numpy.pi/3
+    FLfeq = 1.9
+    FLoff = 0.19
+
+
     physicsClient = p.connect(p.GUI)
     p.setAdditionalSearchPath(pybullet_data.getDataPath())
     p.setGravity(0,0,-9.8)
@@ -12,9 +21,13 @@ def main():
     p.loadSDF("world.sdf")
     robotId = p.loadURDF("body.urdf")
     pyrosim.Prepare_To_Simulate(robotId)
-    backLegSensorValues = numpy.zeros(10000)
-    frontLegSensorValues = numpy.zeros(10000)
-    for i in range(10000):
+    backLegSensorValues = numpy.zeros(1000)
+    backLegMotorControl = BLamp*numpy.sin(numpy.array(range(0,1000)) * 
+numpy.pi / 180 * BLfeq + BLoff)
+    frontLegMotorControl = FLamp*numpy.sin(numpy.array(range(0,1000)) *
+numpy.pi / 180 * FLfeq + FLoff)
+    frontLegSensorValues = numpy.zeros(1000)
+    for i in range(1000):
         if i%100==0:
             print(i)
         p.stepSimulation()
@@ -22,7 +35,15 @@ def main():
         backLegSensorValues[i] = backLegTouch
         frontLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
         frontLegSensorValues[i] = frontLegTouch
-        time.sleep(1/10000)
+        pyrosim.Set_Motor_For_Joint(bodyIndex = robotId,
+            jointName = b'Torso_BackLeg',controlMode = p.POSITION_CONTROL,
+            targetPosition = backLegMotorControl[i],maxForce = 
+90)
+        pyrosim.Set_Motor_For_Joint(bodyIndex = robotId,
+            jointName = b'Torso_FrontLeg',controlMode = p.POSITION_CONTROL,
+            targetPosition = frontLegMotorControl[i],maxForce = 
+90)
+        time.sleep(1/1000)
     p.disconnect()
     numpy.save("data/backLegData.npy",backLegSensorValues)
     numpy.save("data/frontLegData.npy",frontLegSensorValues)
